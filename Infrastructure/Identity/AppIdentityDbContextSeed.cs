4e85dfd552fefe13aaaf0f1d69149bbadcaea05d
Bruno Alves <brunoalveslp200597@gmail.com>
2023-12-03 11:54:18 -0300

    User, Address e Identity Db Context

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity;
+
+public class AppIdentityDbContextSeed
+{
+    public static async Task SeedUserAsync(UserManager<User> userManager)
+    {
+        if(!userManager.Users.Any())
+        {
+            var user = new User
+            {
+                DisplayName = "Admin",
+                Email = "admin@test.com",
+                UserName = "admin@test.com",
+                Address = new Address
+                {
+                    FirstName = "Admin",
+                    LastName = "Bruno",
+                    Street = "Rua Qualquer Nº100",
+                    City = "Brusque",
+                    State = "SC",
+                    PostalCode = "00000-000"
+                }
+            };
+
+            await userManager.CreateAsync(user, "Pa$$w0rd");
+        }
+    }
+}
