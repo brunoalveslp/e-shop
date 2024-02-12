060c9add457a9ed23b9ca0866f51289a2f242bab
Bruno Alves <brunoalveslp200597@gmail.com>
2024-02-12 10:38:49 -0300

    Account page

---
+++ Client/src/app/pages/account/register/register.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
+import { debounceTime, finalize, map, switchMap, take } from 'rxjs';
+import { Address } from 'src/app/shared/models/user';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+  errors: string[] | null = null;
+
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) {}
+
+  complexPassword = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$"
+
+  address = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    street: ['', Validators.required],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    postalCode: ['', Validators.required],
+  })
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email],[this.validateEmailNotTaken()]],
+    roles: [['User']],
+    password: ['', [Validators.required, Validators.pattern(this.complexPassword)]],
+    address: this.address
+  })
+
+  onSubmit(){
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+      error: error => this.errors = error.errors
+    })
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(1000),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkIfEmailExists(control.value).pipe(
+            map(result => result ? {emailExists: true} : null),
+            finalize(() => control.markAsTouched())
+            )
+        })
+      )
+
+    }
+  }
+}
