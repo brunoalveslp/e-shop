a05af95f7464fb265cf8188f948a1e3eacbe97fa
Bruno Alves <brunoalveslp200597@gmail.com>
2024-02-12 10:38:22 -0300

    Order totals

---
+++ Client/src/app/shared/order-totals/order-totals.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CartService } from 'src/app/pages/cart/cart.service';
+
+@Component({
+  selector: 'app-order-totals',
+  templateUrl: './order-totals.component.html',
+  styleUrls: ['./order-totals.component.scss']
+})
+export class OrderTotalsComponent {
+  constructor(public cartService: CartService){}
+}
