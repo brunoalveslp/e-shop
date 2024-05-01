18d2ad9ed3b5710e37562e85718a9fcd9493696e
Bruno Alves <brunoalveslp200597@gmail.com>
2024-05-01 20:33:05 -0300

    Checkout updates

--- Client/src/app/pages/checkout/checkout-payment/checkout-payment.component.ts
+++ Client/src/app/pages/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { CheckoutService } from '../checkout.service';
 import { ToastrService } from 'ngx-toastr';
@@ -6,45 +6,139 @@ import { Cart } from 'src/app/shared/models/cart';
 import { Address } from 'src/app/shared/models/user';
 import { NavigationExtras, Router } from '@angular/router';
 import { CartService } from '../../cart/cart.service';
+import { Stripe, StripeCardCvcElement, StripeCardExpiryElement, StripeCardNumberElement, loadStripe } from '@stripe/stripe-js';
+import { firstValueFrom } from 'rxjs';
+import { OrderToCreate } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent {
+export class CheckoutPaymentComponent implements OnInit{
   @Input() checkoutForm?: FormGroup;
+  @ViewChild('cardNumber') cardNumberElement?: ElementRef;
+  @ViewChild('cardExpiry') cardExpiryElement?: ElementRef;
+  @ViewChild('cardCVC') cardCvcElement?: ElementRef;
+  stripe: Stripe | null = null;
+  cardNumber?: StripeCardNumberElement;
+  cardExpiry?: StripeCardExpiryElement;
+  cardCvc?: StripeCardCvcElement;
+  CardNumberComplete: boolean = false;
+  CardExpiryComplete: boolean = false;
+  CardCvcComplete: boolean = false;
+  cardErrors: any;
+  loading: boolean = false;
 
   constructor(private cartService: CartService,
     private checkoutService: CheckoutService,
     private toastr: ToastrService,
     private router: Router){}
 
+  ngOnInit(){
+    loadStripe('pk_test_51Om2WKFYstaow4m6KfJj2wUSpP1bSN3tpmcODZ1VIhUBROorNrCJApeWZOmbelyop5RGgW6OxfRYGp2oenFIg7SN00CDM2mNSQ')
+      .then(stripe => {
+        this.stripe = stripe;
+        const elements = stripe?.elements();
+        if(elements){
+          this.cardNumber = elements.create('cardNumber');
+          this.cardNumber.mount(this.cardNumberElement?.nativeElement);
+          this.cardNumber.on('change', (event) => {
+            this.CardNumberComplete = event.complete;
+            if(event.error){
+              this.cardErrors = event.error.message;
+            } else {
+              this.cardErrors = null;
+            }
+          });
+          this.cardExpiry = elements.create('cardExpiry');
+          this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
+          this.cardExpiry.on('change', (event) => {
+            this.CardExpiryComplete = event.complete;
+            if(event.error){
+              this.cardErrors = event.error.message;
+            } else {
+              this.cardErrors = null;
+            }
+          });
+          this.cardCvc = elements.create('cardCvc');
+          this.cardCvc.mount(this.cardCvcElement?.nativeElement);
+          this.cardCvc.on('change', (event) => {
+            this.CardCvcComplete = event.complete;
+            if(event.error){
+              this.cardErrors = event.error.message;
+            } else {
+              this.cardErrors = null;
+            }
+          });
 
-  submitOrder(){
+        }
+
+      })
+  }
+
+
+  get paymentFormComplete(){
+    return this.checkoutForm?.get('paymentForm')?.valid && this.CardNumberComplete && this.CardExpiryComplete && this.CardCvcComplete;
+  }
+
+  async submitOrder(){
+    this.loading = true;
     const cart = this.cartService.getCurrentCartValue();
 
-    if(!cart) return;
+    try {
+      const createdOrder = await this.createOrder(cart);
+      const paymentResult = await this.confirmPaymentWithStripe(cart);
+
+      if(paymentResult.paymentIntent){
+        if(cart){
+          this.cartService.deleteCart(cart);
+        }
+        const navigationExtras: NavigationExtras = {state: createdOrder}
+        this.router.navigate(['checkout/success'], navigationExtras);
+      } else {
+        this.toastr.error(paymentResult.error.message);
+      }
+
+    } catch (error: any) {
+      console.log(error);
+      this.toastr.error(error.message);
+    } finally {
+      this.loading = false;
+    }
+
+  }
+
+  private confirmPaymentWithStripe(cart: Cart | null) {
+    if(!cart) throw new Error('O carrinho é null.');
+
+    const result = this.stripe?.confirmCardPayment(cart.clientSecret!, {
+      payment_method: {
+        card: this.cardNumber!,
+        billing_details: {
+          name: this.checkoutForm?.get('paymentForm')?.get('nameOnCard')?.value
+        }
+      }
+    });
+
+    if(!result) throw new Error('Problemas ao tentar realizar o pagamento com na Stripe.');
+
+    return result;
+  }
+
+  private async createOrder(cart: Cart | null) {
+    if(!cart) throw new Error('O carrinho é null.');
 
     const orderToCreate = this.getOrderToCreate(cart);
 
-    if(!orderToCreate) return;
-
-    this.checkoutService.createOrder(orderToCreate).subscribe({
-      next: order => {
-        this.toastr.success('Order created successfully');
-        this.cartService.deleteLocalCart();
-        const navigationExtras: NavigationExtras = {state: order}
-        this.router.navigate(['checkout/success'], navigationExtras);
-      }
-    })
+    return firstValueFrom(this.checkoutService.createOrder(orderToCreate));
   }
 
-  private getOrderToCreate(cart: Cart) {
+  private getOrderToCreate(cart: Cart) : OrderToCreate {
     const deliveryMethodId = this.checkoutForm?.get('deliveryForm')?.get('deliveryMethod')?.value;
     const shipToAddress = this.checkoutForm?.get('addressForm')?.value as Address;
 
-    if(!deliveryMethodId || !shipToAddress) return;
+    if(!deliveryMethodId || !shipToAddress) throw new Error('O método de entrega ou o endereço de entrega não foram selecionados.');
 
     return {
       cartId: cart.id,
--- Client/src/app/pages/checkout/checkout-review/checkout-review.component.ts
+++ Client/src/app/pages/checkout/checkout-review/checkout-review.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CartService } from '../../cart/cart.service';
+import { ToastrService } from 'ngx-toastr';
+import { CdkStepper } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-checkout-review',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout-review.component.scss']
 })
 export class CheckoutReviewComponent {
+  @Input() appStepper?: CdkStepper;
 
+
+  constructor(private cartService: CartService, private toastr: ToastrService) { }
+
+  createPaymentIntent() {
+    this.cartService.createPaymentIntent().subscribe({
+      next: () => {
+        this.toastr.success('Intenlção de pagamento criada com sucesso!');
+        this.appStepper?.next();
+      },
+      error: error => {
+        this.toastr.error(error.message);
+      }
+    });
+  }
 }
